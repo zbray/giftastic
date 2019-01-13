7e6fe790d41558efede1705cb94e801ef94b5e97
Cold Zebra <zachdbray@gmail.com>
2019-01-13 16:29:29 -0600

    4:30pm

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 
+//Variables 
+var topics = ["general organa", "star wars rey", "star wars finn", "han solo", "luke skywalker", "lando calrissian"]
+
+// function 
 
 })
