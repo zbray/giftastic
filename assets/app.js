efc35a64864c30a31c543edd4184bf63f26378ae
Cold Zebra <zachdbray@gmail.com>
2019-01-13 17:54:13 -0600

    added queryURL and ajax GET call

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){
 
-//Variables 
+//Per instructions, a themed array of gif search
 var characters = ["general organa", "star wars rey", "star wars finn", "han solo", "luke skywalker", "lando calrissian"]
 
-// function 
+// Function for Displaying User-Generated Character Names
+function displayGifs(){
+  var char = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=N5OKJJ5Kx7gKlaYEZha1x3zZvZli3Wwd&tag=" + char;
+
+  
+}
+
+// Function for Displaying Gifs
 
 //On button click
 $("#userQuery").on("click", function(event){
   event.preventDefault();
   var charName = $("#userQuery").val().trim();
+  characters.push(charName);
+  renderGifs();
 
-})
+});
+
+//Calling the displayGifs function everytime an element with the charGif id is clicked.
+$(document).on("click", ".charGif", displayGifs);
+
+
 
 })
