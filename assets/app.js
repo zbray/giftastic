79cb9df25b11c62d80631d03370cf6383d667192
Cold Zebra <zachdbray@gmail.com>
2019-01-14 10:13:49 -0600

    10:13a for loop draft

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,38 @@ var characters = ["general organa", "star wars rey", "star wars finn", "han solo
 // Function for Displaying User-Generated Character Names
 function displayGifs(){
   var char = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=N5OKJJ5Kx7gKlaYEZha1x3zZvZli3Wwd&tag=" + char;
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=N5OKJJ5Kx7gKlaYEZha1x3zZvZli3Wwd&tag=" + char + "&limit=10";
 
-  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){
+    var results = response.data;
+    var gifURL = response.data.image_original_url;
+
+    For loop to return create gifs for each user query
+    for (var i = 0; i < results.length; i++) {
+      var gifDiv = $("<div>");
+
+      var rating = results[i].rating;
+
+      var p = $("<p>").text("Rating: " + rating);
+
+      var gifImage = $("<img>");
+      gifImage.attr("src", results[i].images.fixed_height.url);
+
+      gifDiv.prepend(p);
+      gifDiv.prepend(gifImage);
+
+      $("#gifsview").prepend(gifDiv);
+    }
+
+    gifImage.attr("src", imageUrl);
+    gifImage.attr("alt", "punch it, chewey");
+
+    $("#gifsview").prepent(gifDvi);
+    
+  })
 }
 
 // Function for Displaying Gifs
